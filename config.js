2872a6638503eb05a05fda59db9dfd449f0246e5
NB1986 <79172720+NB1986@users.noreply.github.com>
2021-07-07 17:50:27 -0600

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '1216334028815478',
-        clientSecret: '4832a6b3d9d0a18e977006346beb1a82'
-    }
-}
