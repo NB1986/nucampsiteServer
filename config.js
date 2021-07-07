ceff5045ea2f50c09b201760eba4b3f6f152c725
Nick Boucher <nboucher866@gmail.com>
2021-07-07 16:40:33 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '1216334028815478',
+        clientSecret: '4832a6b3d9d0a18e977006346beb1a82'
+    }
 }
